ed50eaf0309320d191acd13aa5c4095774488440
Aminu Ibrahim <aminu21@gmail.com>
2019-05-19 18:37:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('reques')
+
+const app = express()
+
+app.set('port', (process.env.PORT || '5000'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//routes
+app.get('/', function(req, res){
+  res.send('Hi! I am a chatbot')
+})
+
+//fb
+app.get('/webhook/', function(req,res){
+  if(req.query['hub.verify_token'] === ''){
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('wrong token')
+}
+
+app.listen(app.get('port'), function() {
+  console.log('running: ', port)
+})
+
